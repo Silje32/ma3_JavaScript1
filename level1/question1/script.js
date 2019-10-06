215c61ae5b417a6866989ec9c627a02fa2233ba2
siljeheldalmonsen <silje24@yahoo.com>
2019-10-06 23:11:59 +0200

    first commit

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,52 @@
 //Use RegEx to validate form
+
+//Validating that "name" and "last name" inputs are not empty.
+function checkName(name) {
+  var myName = /(\w+)/;
+  if (name.match(myName)) {
+     return true;
+  }  else {
+     alert( "The name entered is invalid!" );
+     return false;
+  }
+}
+function checkLastName(lastname) {
+  var myLastName = /(\w+)/;
+  if (lastname.match(myLastName)) {
+     return true;
+  }  else {
+     alert( "The last name entered is invalid!" );
+     return false;
+  }
+}
+
+//Validating that the phone and e-mail adress are correct.
+function checkPhone(phone) {
+  var myPhone = /^d\d\d\d\d\d\d\d$/;
+  if (phone.match(myPhone)) {
+     return true;
+  }  else {
+     alert( "The phone number entered is invalid!" );
+     return false;
+  }
+}
+function checkEmail(email) {
+  var myEmail = /\w+\.\w+\/;
+  if (email.match(myEmail)) {
+     return true;
+  }) else {
+     alert( "The e-mail number entered is invalid!" );
+     return false;
+  }
+}
+
+//Function to check the submit button.
+function checkSubmitBtn (submit) {
+  var mySubmitBtn = testInfo(document.getElementById('submit'));
+  if (submit === true) {
+     console.log("True");
+}    else {
+     console.log("False");
+  }
+}
+checkSubmitBtn();
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,30 @@
 //Console.log out elements in JSON file
+var info = {
+    "video": [{
+        "id": 12312412312,
+        "name": "Ecuaciones Diferenciales",
+        "url": "/video/math/edo/12312412312",
+        "author": {
+            "data": [{
+                "name_author": "Alejandro Morales",
+                "url": "/author/alejandro-morales",
+                "type": "master"
+            }]
+        }
+    }]
+}
+
+//For each array element
+var output = "";
+  for (var i = 0; i <= info.video.length-1; i++) {
+     for (key in info.video[i]) {
+       if (info.video[i].hasOwnProperty(key)) {
+          console.log(output);
+       if (info.video.author.hasOwnProperty(key)) {
+         for (key in info.video.author.data[i]) {
+             console.log(output);
+    }
+   }
+  }
+ }
+}
